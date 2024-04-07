363ca92da6ddc9a584704199c7b474412888627c
Sean-Randall <sean.r.randall@gmail.com>
2024-04-07 00:53:24 -0400

    This is my first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # atm-project
 A program to check balance or withdraw money from an ATM
+
+This is a project created in **ASD101A**
+
+
---
+++ atm.py
@@ -0,0 +1,4 @@
+# This is a program to allow a user to check 
+# their balance or withdraw money form an ATM.
+
+print("Welcome to the ATM!")
